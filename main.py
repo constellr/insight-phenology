420e0b23ea0eef6ea9fb93279e0f65710fd232ad
Dimo Dimov <dimo.dimov@constellr.com>
2024-11-15 11:54:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import pandas as pd
 import geopandas as gpd
 
 from phenology import *
-from cropland import *
+from canopy import *
 from plotting import *
+from gdd import *
 
 pd.set_option('mode.chained_assignment', None)
 
 if __name__ == '__main__':
 
     # rasterfiles
-    dir_ndvi = r"folder-to-NDVI"
-    dir_clip = r"folder-to-clipped-NDVI"
-    dir_lsti = r"folder-to-LST"
+    dir_ndvi = r"C:\Users\DimoDimov\Documents\DATA\LST_Syngenta\Landsat\NDVI"
+    dir_clip = r"C:\Users\DimoDimov\Documents\DATA\LST_Syngenta\Landsat\NDVI\CLIP"
+    dir_lsti = r"C:\Users\DimoDimov\Documents\DATA\LST_Syngenta\LST"
 
     # input data
-    aoi = r"syngenta_parcels.geojson"
-    utm = r"utm_zones.geojson"
+    aoi = r"C:\Users\DimoDimov\Documents\DATA\LST_Syngenta\syngenta_parcels.geojson"
+    utm = r"C:\Users\DimoDimov\Documents\DATA\utm_zones.geojson"
 
     # output
     output_csv = r"phenology_gdd.csv"
@@ -28,7 +29,7 @@ if __name__ == '__main__':
 
     ### GDD parameters
     Tbase = 10
-    Tmax = 25
+    Tmax = 40
     Tmin = 10
 
     aoi = get_crs(aoi,utm)
@@ -36,44 +37,68 @@ if __name__ == '__main__':
     gids = aoi.id.unique()
     for gid in gids:
 
-        try:
+        ### NDVI ###
 
-            ### NDVI ###
+        veg = aggregation(dir_ndvi, aoi, gid)
+        df = preprocess_df(veg)
+        df = smoothing(df)
 
-            veg = aggregation(dir_ndvi,aoi,gid)
-            df = preprocess_df(veg)
-            df = smoothing(df)
+        if len(df) > 0:
 
-            if len(df) > 0:
+            peak_dates, peak_values, pos_date, pos_value = get_peaks(df)
 
-                peaks, peak_dates, max_val = get_peaks(df)
-                sos, pos, eos = get_markers(df,peak_dates)
-                plateaus = get_plateau(df)
-                inflection_points, acceleration_1, acceleration_2, acceleration_3, acceleration_4 = get_derivatives(df,sos,pos,eos)
-                rate = get_growth_rate(acceleration_3,acceleration_1)
+            try:
+                sos_date, sos_value, eos_date, eos_value = get_markers(df, pos_date)
+            except:
+                sos_date = df['date'].head(1).values[0]
+                sos_value = df['filter'].head(1).values[0]
+                eos_date = df['date'].tail(1).values[0]
+                eos_value = df['filter'].tail(1).values[0]
 
-                ### LST ###
+            inflection_points, acceleration_points = get_derivatives(df, sos_date, pos_date, eos_date)
+            growth_rate = get_growth_rate(sos_value, pos_value)
+            plateaus = get_plateau(df)
 
-                lst = aggregation(dir_lsti,aoi,gid)
-                lst = preprocess_df(lst)
-                gdd = get_gdd(lst,sos,eos,Tbase,Tmax,Tmin)
+            ### LST ###
 
-                ### merge both dataframes on date
+            lst = aggregation(dir_lsti, aoi, gid)
+            lst = preprocess_df(lst)
 
-                df['NDVI'] = df['gaussian']
-                df = df.drop(columns=['id', 'mean', 'min', 'max', 'doy', 'interpol', 'gaussian'])
-                gdd = gdd.drop(columns=['id', 'mean', 'min', 'max', 'doy', 'Tmax', 'interpol', 'gaussian'])
+            gdd = get_gdd(
+                lst,
+                sos_date,
+                pos_date,
+                eos_date,
+                inflection_points,
+                acceleration_points
+            )
 
-                merge = pd.merge(gdd, df, on='date')
-                merge["id"] = gid
-                merge["growth_rate"] = rate
-                merge["SOS"] = sos
-                merge["POS"] = pos
-                merge["EOS"] = eos
+            gdd['id'] = gid
 
-                final_df = pd.concat([merge,final_df])
+            ### merge both dataframes on date
 
-        except:
-            pass
+            df['NDVI'] = df['filter']
+            df = df.drop(columns=['mean', 'min', 'max', 'doy', 'interpol', 'filter'])
+
+            ard_df = df.groupby('id').agg(
+                sum_ndvi=('NDVI', 'sum'),
+                std_ndvi=('NDVI', 'std'),
+                mean_ndvi=('NDVI', 'mean')
+            ).reset_index()
+
+            ard_df["growth_rate"] = growth_rate
+            ard_df["SOS"] = sos_date
+            ard_df["POS"] = pos_date
+            ard_df["EOS"] = eos_date
+
+            for i in range(0,len(inflection_points)):
+                if i == 2:
+                    ard_df[f"inflection_date_{i}"] = inflection_points['date'].values[i]
+                    ard_df[f"inflection_value_{i}"] = inflection_points['filter'].values[i]
+                break
+
+            merge = pd.merge(ard_df, gdd, on='id')
+
+            final_df = pd.concat([merge,final_df])
 
     final_df.to_csv(output_csv, sep=';')
