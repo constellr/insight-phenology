13c1a2a283ce1c3219d505ef1f09449042ec4ed8
Dimo Dimov <dimo.dimov@constellr.com>
2024-11-21 11:56:48 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,5 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+
+def plot_gdd(gdd):
+
+    fig, ax1 = plt.subplots()
+
+    ax1.plot(gdd['date'], gdd['GDD'], color='blue', label='GDD')
+    ax1.set_xlabel('Year')
+    ax1.set_ylabel('GDD', color='red')
+    ax1.tick_params(axis='y', labelcolor='blue')
+
+    ax2 = ax1.twinx()
+    ax2.plot(gdd['date'], gdd['interpol_x'], color='green', label='NDVI')
+    ax2.set_ylabel('NDVI', color='green')
+    ax2.tick_params(axis='y', labelcolor='green')
+
+    fig.legend(loc='upper left', bbox_to_anchor=(0.1, 0.9))
+
+    unique_stages = sorted(gdd['stage'].unique())
+    colors = ['black', 'blue', 'green', 'yellow', 'orange']
+    if len(unique_stages) < len(colors):
+        colors = colors[:len(unique_stages)]
+
+    for stage, color in zip(unique_stages, colors):
+        stage_indices = gdd[gdd['stage'] == stage]['date']
+        if len(stage_indices) > 0:
+            ax1.axvspan(stage_indices.min(), stage_indices.max(), color=color, alpha=0.3, label=stage)
+
+    ax1.set_title("GDD-derived crop growth stages")
+    ax1.set_xlabel("date")
+    ax1.set_ylabel("GDD")
+
+    plt.show()
 
 def plot_phenology(
         ndvi_ts, gdd_ts, plateaus, inflection_points,
@@ -8,7 +41,7 @@ def plot_phenology(
     ):
 
     ndvi_interpol = ndvi_ts['interpol'].values
-    ndvi_gaussian = ndvi_ts['gaussian'].values
+    ndvi_filter = ndvi_ts['filter'].values
     ndvi_dates = ndvi_ts['date'].values
 
     gdd_dates = gdd_ts['date'].values
@@ -16,7 +49,7 @@ def plot_phenology(
 
     fig, ax1 = plt.subplots()
     ax1.plot(ndvi_dates, ndvi_interpol, label='Raw NDVI', color='blue', linewidth=1.5)
-    ax1.plot(ndvi_dates, ndvi_gaussian, label='Smooth NDVI', color='lightgreen', linewidth=1.5)
+    ax1.plot(ndvi_dates, ndvi_filter, label='Smooth NDVI', color='lightgreen', linewidth=1.5)
 
     ax2 = ax1.twinx()
     ax2.plot(gdd_dates, gdd_values, 'r-', label='GDD')
@@ -26,20 +59,20 @@ def plot_phenology(
 
     plt.figure(figsize=(12, 6))
     plt.plot(ndvi_dates, ndvi_interpol, label='Raw NDVI', color='blue', linewidth=1.5)
-    plt.plot(ndvi_dates, ndvi_gaussian, label='Smooth NDVI', color='lightgreen', linewidth=1.5)
+    plt.plot(ndvi_dates, ndvi_filter, label='Smooth NDVI', color='lightgreen', linewidth=1.5)
 
     for idx, plateau in enumerate(plateaus):
         start_date = ndvi_ts.loc[plateau[0], 'date']
         end_date = ndvi_ts.loc[plateau[-1] + 1, 'date'] if plateau[-1] + 1 < len(ndvi_ts) else ndvi_ts.loc[plateau[-1], 'date']
         plt.axvspan(start_date, end_date, color='yellow', alpha=0.3, label='Plateaus')
 
-    plt.scatter(inflection_points['date'], inflection_points['gaussian'], color='black', label='Stage Change',zorder=5)
-    plt.scatter(acceleration_1['date'], acceleration_1['gaussian'], color='green', label='Canopy Closing',zorder=5)
-    plt.scatter(acceleration_3['date'], acceleration_3['gaussian'], color='red', label='Start vegetative period', zorder=5)
+    plt.scatter(inflection_points['date'], inflection_points['filter'], color='black', label='Stage Change',zorder=5)
+    plt.scatter(acceleration_1['date'], acceleration_1['filter'], color='green', label='Canopy Closing',zorder=5)
+    plt.scatter(acceleration_3['date'], acceleration_3['filter'], color='red', label='Start vegetative period', zorder=5)
 
-    plt.plot(peak_dates, ndvi_ts['gaussian'].iloc[peaks], "x", color='red', label='Peaks')
-    plt.scatter(acceleration_2['date'], acceleration_2['gaussian'], color='orange', label='Start of Maturity',zorder=5)
-    plt.scatter(acceleration_4['date'], acceleration_4['gaussian'], color='magenta', label='Start of Senescence', zorder=5)
+    plt.plot(peak_dates, ndvi_ts['filter'].iloc[peaks], "x", color='red', label='Peaks')
+    plt.scatter(acceleration_2['date'], acceleration_2['filter'], color='orange', label='Start of Maturity',zorder=5)
+    plt.scatter(acceleration_4['date'], acceleration_4['filter'], color='magenta', label='Start of Senescence', zorder=5)
 
     plt.axvline(sos, color="forestgreen", linestyle="-.", label=f"Start of Season (SOS)")
     plt.axvline(pos, color="forestgreen", linestyle="solid", label=f"Peak of Season (POS)")
